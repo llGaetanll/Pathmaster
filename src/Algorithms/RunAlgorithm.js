3c794db4ea5755e746e144221dd32f9bb4781bcd
Andrew Penton <apenton@ufl.edu>
2020-11-15 20:53:06 -0600

    added option to show all squares for rook/bishop/queen

--- src/Algorithms/RunAlgorithm.js
+++ src/Algorithms/RunAlgorithm.js
@@ -23,12 +23,58 @@ export function runAlgorithm(alg, grid, start, end, pieceType) {
 
 // Backtracks from the finishNode to find the shortest path.
 // Works for any search alg so this method can be used for all of them
-export function getNodesInOrder(finish) {
+export function getNodesInOrder(finish, pieceType, showAll, grid) {
 	let inOrder = [];
 	let curr = finish;
-	while (curr !== null) {
-		inOrder.unshift(curr);
-		curr = curr.previousNode;
+	if (showAll && pieceType !== "Knight") {
+		let curr = finish.previousNode;
+		let lastNode = finish;
+		while (curr !== null) {
+			while (lastNode !== curr) {
+				inOrder.unshift(lastNode);
+				lastNode = getAdjacent(lastNode, curr, grid);
+			}
+			inOrder.unshift(lastNode);
+			curr = curr.previousNode;
+		}
+	}
+	else {
+		while (curr !== null) {
+			inOrder.unshift(curr);
+			curr = curr.previousNode;
+		}
 	}
 	return inOrder;
+}
+
+//returns next adjacent node in a row/column/diagonal
+function getAdjacent(lastNode, curr, grid) {
+	//if horizontal, get next horizontal node
+	if (lastNode.row === curr.row) {
+		if (lastNode.col < curr.col)
+			return grid[lastNode.row][lastNode.col+1];
+		else
+			return grid[lastNode.row][lastNode.col-1];
+	}
+	//vertical
+	if (lastNode.col === curr.col) {
+		if (lastNode.row < curr.row)
+			return grid[lastNode.row+1][lastNode.col];
+		else
+			return grid[lastNode.row-1][lastNode.col];
+	}
+	//diag1
+	if (lastNode.row + lastNode.col === curr.row + curr.col) {
+		if (lastNode.row - lastNode.col < curr.row - curr.col)
+			return grid[lastNode.row+1][lastNode.col-1];
+		else
+			return grid[lastNode.row-1][lastNode.col+1];
+	}
+	//diag2
+	if (lastNode.row - lastNode.col === curr.row - curr.col) {
+		if (lastNode.row + lastNode.col < curr.row + curr.col)
+			return grid[lastNode.row+1][lastNode.col+1];
+		else
+			return grid[lastNode.row-1][lastNode.col-1];
+	}
 }
